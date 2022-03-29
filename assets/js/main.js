ef3212c34af33a4de88256ab4303c51957d1de36
Sushant Rauniyar <sushantrauniyar9841@gmail.com>
2022-03-29 10:56:13 +0545

    table-drag

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,8 +175,8 @@ $(function () {
             $this.closest(".submenu").prev(".sub-dropdown-btn").addClass("active rotate");
             // ".dropdown-btn".addClass("active");
         }
-        if ($(".sub-dropdown-btn").hasClass("active")) {
-            $this.closest(".submenu").prev(".dropdown-btn").addClass("active");
+        if ($(".sub-dropdown-btn").hasClass("active rotate")) {
+            $this.closest(".submenu").prev(".dropdown-btn").addClass("active rotate");
         }
     });
 });
