d77dc4c00ebdbfb3ca7aa3fc1b7925ded547f5b6
Sushant Rauniyar <sushantrauniyar9841@gmail.com>
2022-04-29 17:47:03 +0545

    style

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -101,26 +101,7 @@ function ResetLocalStorage() {
 }
 
 //////////////////////////////////////////////////////////////
-// table-overflow drag
-var elementScroll = document.querySelector(".table-overflow");
-var tableoverflow = 0;
-
-elementScroll.addEventListener("mousedown", function (e) {
-    this.sx = this.scrollLeft;
-    tableoverflow = e.pageX - this.offsetLeft;
-
-    this.addEventListener("mousemove", mouseMoveFunction);
-});
-
-elementScroll.addEventListener("mouseup", function (e) {
-    this.removeEventListener("mousemove", mouseMoveFunction);
-    tableoverflow = 0;
-});
-
-function mouseMoveFunction(e) {
-    var tableoverflow2 = e.pageX - this.offsetLeft;
-    if (tableoverflow) this.scrollLeft = this.sx + tableoverflow - tableoverflow2;
-}
+// color picker for main theme color
 
 var root = document.querySelector(":root");
 
